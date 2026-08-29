7d75abfa8d0c0d3cc9424ef6e5e8e54ab4cdb284
Victor Edeh <edehvictor715@gmail.com>
2026-08-29 12:15:17 +0100

    feat(terraform): add Redis failover variables

--- deployment/terraform/modules/elasticache/variables.tf
+++ deployment/terraform/modules/elasticache/variables.tf
@@ -37,3 +37,25 @@ variable "auth_token_enabled" {
   type        = bool
   default     = true
 }
+variable "replica_count" {
+  description = "Number of Redis read replicas. Set to at least 1 to enable automatic failover and Multi-AZ placement."
+  type        = number
+  default     = 1
+
+  validation {
+    condition     = var.replica_count >= 1
+    error_message = "replica_count must be at least 1 so ElastiCache can fail over to a replica in another subnet/AZ."
+  }
+}
+
+variable "at_rest_encryption_enabled" {
+  description = "Enable at-rest encryption for the Redis replication group."
+  type        = bool
+  default     = true
+}
+
+variable "apply_immediately" {
+  description = "Apply Redis replication group changes immediately instead of during the next maintenance window."
+  type        = bool
+  default     = true
+}
