d9953d1d8248cb4fbbefa43537ab5509ed67cf4d
Zilla <emmanueligbokwe20@gmail.com>
2026-08-31 21:44:35 +0100

    feat: [FEATURE] Add AWS cost/budget alerting (#1295)

---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Deployment environment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "project" {
+  description = "Project name used for tagging and resource naming"
+  type        = string
+  default     = "stellar-portfolio-rebalancer"
+}
+
+variable "budget_limit" {
+  description = "Monthly budget limit in USD"
+  type        = number
+  default     = 100
+}
+
+variable "budget_thresholds" {
+  description = "List of budget thresholds as fractions (e.g., 0.5 for 50%)"
+  type        = list(number)
+  default     = [0.5, 0.8, 0.9]
+}
+
+variable "notification_email" {
+  description = "List of email addresses for budget alerts"
+  type        = list(string)
+  default     = []
+}
