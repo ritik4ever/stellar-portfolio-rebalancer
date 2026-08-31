9dfbe825e780d7b29b8923378a1b79b4ca014d45
Zilla <emmanueligbokwe20@gmail.com>
2026-08-31 21:51:24 +0100

    fix(ci): resolve failing checks for #1295

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -4,8 +4,15 @@ resource "aws_sns_topic" "budget_alerts" {
 }
 
 resource "aws_sns_topic_subscription" "email" {
-  for_each = toset(var.notification_email)
+  for_each = toseet(var.notification_email)
   topic_arn = aws_sns_topic.budget_alerts.arn
   protocol  = "email"
   endpoint  = each.key
+}
+
+resource "aws_sns_topic_subscription" "slack" {
+  count = var.notification_slack_webhook != "" ? 1 : 0
+  topic_arn = aws_sns_topic.budget_alerts.arn
+  protocol  = "https"
+  endpoint  = var.notification_slack_webhook
 }
