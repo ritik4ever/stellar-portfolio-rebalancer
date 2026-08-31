f2be1800eaa1b0e2898d42b333fe9427adae7a43
Zilla <emmanueligbokwe20@gmail.com>
2026-08-31 22:00:26 +0100

    fix(ci): resolve failing checks for #1295

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -4,15 +4,47 @@ resource "aws_sns_topic" "budget_alerts" {
 }
 
 resource "aws_sns_topic_subscription" "email" {
-  for_each = toseet(var.notification_email)
+  for_each = toset(var.notification_email)
   topic_arn = aws_sns_topic.budget_alerts.arn
   protocol  = "email"
-  endpoint  = each.key
+  endpoint = each.key
 }
 
 resource "aws_sns_topic_subscription" "slack" {
   count = var.notification_slack_webhook != "" ? 1 : 0
   topic_arn = aws_sns_topic.budget_alerts.arn
   protocol  = "https"
-  endpoint  = var.notification_slack_webhook
+  endpoint = var.notification_slack_webhook
+}
+
+variable "budget_limit" {
+  description = "Monthly budget limit in USD"
+  type        = number
+  default     = 1000
+}
+
+variable "budget_notification_thresholds" {
+  description = "Percentage thresholds for budget alerts"
+  type        = list(number)
+  default     = [50, 80, 90]
+}
+
+resource "aws_budgets_budget" "monthly" {
+  name         = "monthly-budget-${var.project}-${var.environment}"
+  budget_type  = "COST"
+  limit_amount = var.budget_limit
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+  tags         = local.common_tags
+
+  dynamic "notification" {
+    for_each = var.budget_notification_thresholds
+    content {
+      comparison_operator        = "GREATER_THAN"
+      threshold                  = notification.value
+      threshold_type             = "PERCENTAGE"
+      notification_type          = "ACTUAL"
+      subscriber_sns_topic_arns  = [aws_sns_topic.budget_alerts.arn]
+    }
+  }
 }
